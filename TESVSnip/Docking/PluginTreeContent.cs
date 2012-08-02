b172597aaf85603fae1948cc72cad85de5ec46e3
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-08-02 04:28:00 +0000

    Adding Files for Figments TESVSnip v4.2 from his Git Hub

---
+++ TESVSnip/Docking/PluginTreeContent.cs
@@ -0,0 +1,15 @@
+﻿namespace TESVSnip.Docking
+{
+    public partial class PluginTreeContent : WeifenLuo.WinFormsUI.Docking.DockContent
+    {
+        public PluginTreeContent()
+        {
+            InitializeComponent();
+        }
+
+        public TESVSnip.ObjectControls.PluginTreeView PluginTree
+        {
+            get { return this.pluginTreeControl; }
+        }
+    }
+}
