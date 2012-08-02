34d56533be26e0bf8eb4ad01b86f48f1a77c5d87
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-08-02 05:49:50 +0000

    - Major Changes to Strings Editor

--- TESVSnip/Forms/StringsEditor.Designer.cs
+++ TESVSnip/Forms/StringsEditor.Designer.cs
@@ -39,7 +39,6 @@
             this.tip = new System.Windows.Forms.ToolTip(this.components);
             this.btnLookup = new System.Windows.Forms.Button();
             this.btnDeleteString = new System.Windows.Forms.Button();
-            this.error = new System.Windows.Forms.ErrorProvider(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.cboPlugins = new System.Windows.Forms.ComboBox();
             this.btnClose = new System.Windows.Forms.Button();
@@ -48,24 +47,18 @@
             this.btnNewItem = new System.Windows.Forms.Button();
             this.listStrings = new TESVSnip.Windows.Controls.BindingListView();
             this.btnApply = new System.Windows.Forms.Button();
+            this.error = new System.Windows.Forms.ErrorProvider(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.error)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             resources.ApplyResources(this.label1, "label1");
-            this.error.SetError(this.label1, resources.GetString("label1.Error"));
-            this.error.SetIconAlignment(this.label1, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("label1.IconAlignment"))));
-            this.error.SetIconPadding(this.label1, ((int)(resources.GetObject("label1.IconPadding"))));
             this.label1.Name = "label1";
-            this.tip.SetToolTip(this.label1, resources.GetString("label1.ToolTip"));
             // 
             // btnAddString
             // 
             resources.ApplyResources(this.btnAddString, "btnAddString");
-            this.error.SetError(this.btnAddString, resources.GetString("btnAddString.Error"));
-            this.error.SetIconAlignment(this.btnAddString, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnAddString.IconAlignment"))));
-            this.error.SetIconPadding(this.btnAddString, ((int)(resources.GetObject("btnAddString.IconPadding"))));
             this.btnAddString.Image = global::TESVSnip.Properties.Resources.edit_add;
             this.btnAddString.Name = "btnAddString";
             this.tip.SetToolTip(this.btnAddString, resources.GetString("btnAddString.ToolTip"));
@@ -75,38 +68,23 @@
             // txtID
             // 
             resources.ApplyResources(this.txtID, "txtID");
-            this.error.SetError(this.txtID, resources.GetString("txtID.Error"));
-            this.error.SetIconAlignment(this.txtID, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("txtID.IconAlignment"))));
-            this.error.SetIconPadding(this.txtID, ((int)(resources.GetObject("txtID.IconPadding"))));
             this.txtID.Name = "txtID";
-            this.tip.SetToolTip(this.txtID, resources.GetString("txtID.ToolTip"));
             this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
             this.txtID.Validating += new System.ComponentModel.CancelEventHandler(this.txtID_Validating);
             // 
             // txtString
             // 
             resources.ApplyResources(this.txtString, "txtString");
-            this.error.SetError(this.txtString, resources.GetString("txtString.Error"));
-            this.error.SetIconAlignment(this.txtString, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("txtString.IconAlignment"))));
-            this.error.SetIconPadding(this.txtString, ((int)(resources.GetObject("txtString.IconPadding"))));
             this.txtString.Name = "txtString";
-            this.tip.SetToolTip(this.txtString, resources.GetString("txtString.ToolTip"));
             // 
             // label2
             // 
             resources.ApplyResources(this.label2, "label2");
-            this.error.SetError(this.label2, resources.GetString("label2.Error"));
-            this.error.SetIconAlignment(this.label2, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("label2.IconAlignment"))));
-            this.error.SetIconPadding(this.label2, ((int)(resources.GetObject("label2.IconPadding"))));
             this.label2.Name = "label2";
-            this.tip.SetToolTip(this.label2, resources.GetString("label2.ToolTip"));
             // 
             // btnEditString
             // 
             resources.ApplyResources(this.btnEditString, "btnEditString");
-            this.error.SetError(this.btnEditString, resources.GetString("btnEditString.Error"));
-            this.error.SetIconAlignment(this.btnEditString, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnEditString.IconAlignment"))));
-            this.error.SetIconPadding(this.btnEditString, ((int)(resources.GetObject("btnEditString.IconPadding"))));
             this.btnEditString.Name = "btnEditString";
             this.tip.SetToolTip(this.btnEditString, resources.GetString("btnEditString.ToolTip"));
             this.btnEditString.UseVisualStyleBackColor = true;
@@ -115,9 +93,6 @@
             // btnLookup
             // 
             resources.ApplyResources(this.btnLookup, "btnLookup");
-            this.error.SetError(this.btnLookup, resources.GetString("btnLookup.Error"));
-            this.error.SetIconAlignment(this.btnLookup, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnLookup.IconAlignment"))));
-            this.error.SetIconPadding(this.btnLookup, ((int)(resources.GetObject("btnLookup.IconPadding"))));
             this.btnLookup.Image = global::TESVSnip.Properties.Resources.find;
             this.btnLookup.Name = "btnLookup";
             this.tip.SetToolTip(this.btnLookup, resources.GetString("btnLookup.ToolTip"));
@@ -127,49 +102,29 @@
             // btnDeleteString
             // 
             resources.ApplyResources(this.btnDeleteString, "btnDeleteString");
-            this.error.SetError(this.btnDeleteString, resources.GetString("btnDeleteString.Error"));
-            this.error.SetIconAlignment(this.btnDeleteString, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnDeleteString.IconAlignment"))));
-            this.error.SetIconPadding(this.btnDeleteString, ((int)(resources.GetObject("btnDeleteString.IconPadding"))));
             this.btnDeleteString.Image = global::TESVSnip.Properties.Resources.edit_remove;
             this.btnDeleteString.Name = "btnDeleteString";
             this.tip.SetToolTip(this.btnDeleteString, resources.GetString("btnDeleteString.ToolTip"));
             this.btnDeleteString.UseVisualStyleBackColor = true;
             this.btnDeleteString.Click += new System.EventHandler(this.btnDeleteString_Click);
             // 
-            // error
-            // 
-            this.error.ContainerControl = this;
-            resources.ApplyResources(this.error, "error");
-            // 
             // label3
             // 
             resources.ApplyResources(this.label3, "label3");
-            this.error.SetError(this.label3, resources.GetString("label3.Error"));
-            this.error.SetIconAlignment(this.label3, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("label3.IconAlignment"))));
-            this.error.SetIconPadding(this.label3, ((int)(resources.GetObject("label3.IconPadding"))));
             this.label3.Name = "label3";
-            this.tip.SetToolTip(this.label3, resources.GetString("label3.ToolTip"));
             // 
             // cboPlugins
             // 
             resources.ApplyResources(this.cboPlugins, "cboPlugins");
             this.cboPlugins.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.error.SetError(this.cboPlugins, resources.GetString("cboPlugins.Error"));
             this.cboPlugins.FormattingEnabled = true;
-            this.error.SetIconAlignment(this.cboPlugins, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("cboPlugins.IconAlignment"))));
-            this.error.SetIconPadding(this.cboPlugins, ((int)(resources.GetObject("cboPlugins.IconPadding"))));
             this.cboPlugins.Name = "cboPlugins";
-            this.tip.SetToolTip(this.cboPlugins, resources.GetString("cboPlugins.ToolTip"));
             // 
             // btnClose
             // 
             resources.ApplyResources(this.btnClose, "btnClose");
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.error.SetError(this.btnClose, resources.GetString("btnClose.Error"));
-            this.error.SetIconAlignment(this.btnClose, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnClose.IconAlignment"))));
-            this.error.SetIconPadding(this.btnClose, ((int)(resources.GetObject("btnClose.IconPadding"))));
             this.btnClose.Name = "btnClose";
-            this.tip.SetToolTip(this.btnClose, resources.GetString("btnClose.ToolTip"));
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -177,31 +132,19 @@
             // 
             resources.ApplyResources(this.cboType, "cboType");
             this.cboType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.error.SetError(this.cboType, resources.GetString("cboType.Error"));
             this.cboType.FormattingEnabled = true;
-            this.error.SetIconAlignment(this.cboType, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("cboType.IconAlignment"))));
-            this.error.SetIconPadding(this.cboType, ((int)(resources.GetObject("cboType.IconPadding"))));
             this.cboType.Name = "cboType";
-            this.tip.SetToolTip(this.cboType, resources.GetString("cboType.ToolTip"));
             // 
             // label4
             // 
             resources.ApplyResources(this.label4, "label4");
-            this.error.SetError(this.label4, resources.GetString("label4.Error"));
-            this.error.SetIconAlignment(this.label4, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("label4.IconAlignment"))));
-            this.error.SetIconPadding(this.label4, ((int)(resources.GetObject("label4.IconPadding"))));
             this.label4.Name = "label4";
-            this.tip.SetToolTip(this.label4, resources.GetString("label4.ToolTip"));
             // 
             // btnNewItem
             // 
             resources.ApplyResources(this.btnNewItem, "btnNewItem");
-            this.error.SetError(this.btnNewItem, resources.GetString("btnNewItem.Error"));
-            this.error.SetIconAlignment(this.btnNewItem, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnNewItem.IconAlignment"))));
-            this.error.SetIconPadding(this.btnNewItem, ((int)(resources.GetObject("btnNewItem.IconPadding"))));
             this.btnNewItem.Image = global::TESVSnip.Properties.Resources.insertcell;
             this.btnNewItem.Name = "btnNewItem";
-            this.tip.SetToolTip(this.btnNewItem, resources.GetString("btnNewItem.ToolTip"));
             this.btnNewItem.UseVisualStyleBackColor = true;
             this.btnNewItem.Click += new System.EventHandler(this.btnNewItem_Click);
             // 
@@ -210,15 +153,11 @@
             resources.ApplyResources(this.listStrings, "listStrings");
             this.listStrings.AutoScroll = false;
             this.listStrings.DataSource = null;
-            this.error.SetError(this.listStrings, resources.GetString("listStrings.Error"));
             this.listStrings.FullRowSelect = true;
             this.listStrings.GridLines = true;
             this.listStrings.HideSelection = false;
-            this.error.SetIconAlignment(this.listStrings, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("listStrings.IconAlignment"))));
-            this.error.SetIconPadding(this.listStrings, ((int)(resources.GetObject("listStrings.IconPadding"))));
             this.listStrings.ItemCount = 0;
             this.listStrings.Name = "listStrings";
-            this.tip.SetToolTip(this.listStrings, resources.GetString("listStrings.ToolTip"));
             this.listStrings.UseCompatibleStateImageBehavior = false;
             this.listStrings.View = System.Windows.Forms.View.Details;
             this.listStrings.VirtualMode = true;
@@ -229,13 +168,13 @@
             // 
             resources.ApplyResources(this.btnApply, "btnApply");
             this.btnApply.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.error.SetError(this.btnApply, resources.GetString("btnApply.Error"));
-            this.error.SetIconAlignment(this.btnApply, ((System.Windows.Forms.ErrorIconAlignment)(resources.GetObject("btnApply.IconAlignment"))));
-            this.error.SetIconPadding(this.btnApply, ((int)(resources.GetObject("btnApply.IconPadding"))));
             this.btnApply.Name = "btnApply";
-            this.tip.SetToolTip(this.btnApply, resources.GetString("btnApply.ToolTip"));
             this.btnApply.UseVisualStyleBackColor = true;
             this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // error
+            // 
+            this.error.ContainerControl = this;
             // 
             // StringsEditor
             // 
@@ -263,7 +202,6 @@
             this.MinimizeBox = false;
             this.Name = "StringsEditor";
             this.ShowInTaskbar = false;
-            this.tip.SetToolTip(this, resources.GetString("$this.ToolTip"));
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StringsEditor_FormClosing);
             this.Load += new System.EventHandler(this.StringsEditor_Load);
             this.ResizeEnd += new System.EventHandler(this.StringsEditor_ResizeEnd);
