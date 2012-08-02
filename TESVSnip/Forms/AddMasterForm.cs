84e7392cb4be06920a4a2bf64a1f41314ff5dbea
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-08-02 04:34:43 +0000

    Adding Files for Figments TESVSnip v4.2 from his Git Hub

---
+++ TESVSnip/Forms/AddMasterForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TESVSnip
+{
+    public partial class AddMasterForm : Form
+    {
+        public string MasterName
+        {
+            get { return tbxMaster.Text; }
+        }
+
+        public AddMasterForm()
+        {
+            InitializeComponent();
+            ofdChooseMaster.InitialDirectory = Path.GetFullPath("data");
+            ofdChooseMaster.FileName = "skyrim.esm";
+            ofdChooseMaster.Filter =
+                "Masters or Plugins (*.esm,*.esp)|*.esm;*.esp|Masters(*.esm)|*.esm|Plugins (*.esp)|*.esp";
+        }
+
+        private void butOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void butChooseMaster_Click(object sender, EventArgs e)
+        {
+            if (ofdChooseMaster.ShowDialog() == DialogResult.OK)
+                tbxMaster.Text = Path.GetFileName(ofdChooseMaster.FileName);
+        }
+    }
+}
