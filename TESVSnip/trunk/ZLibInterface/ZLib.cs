ec1448fb9befde58f121fdbaef8ab57e56f17b84
leandor@gmail.com <leandor@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-08-04 22:55:36 +0000

    [TESVSnip] ZLIB: Added a new folder to implement the wrapper interface between TESVSnip code and the new ZLib implementation.

---
+++ TESVSnip/trunk/ZLibInterface/ZLib.cs
@@ -0,0 +1,28 @@
+namespace TESVSnip.ZLibInterface
+{
+    using System;
+    using System.IO;
+    using DotZLib;
+
+    public static class ZLib
+    {
+        public static BinaryReader Decompress(MemoryStream input, long offset = 0, long size = 0)
+        {
+            if (input == null) {
+                throw new ArgumentNullException("input");
+            }
+
+            if (size == 0) {
+                size = input.Length;
+            }
+
+            if (offset > 0) {
+                var position = input.Position;
+                input.Position = position + offset;
+            }
+
+            var buffer = new byte[size];
+            var inflater = new Inflater(input.)
+        }
+    }
+}
