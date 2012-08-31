069dfe28c756b9a2c3e7c7b18c0a1826df9499f1
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-08-31 21:42:04 +0000

    [TES5Edit] Commit of TED5Edit for Zilav to branches

--- TES5Edit/branches/dev-zilav/TES5Dump.dpr
+++ TES5Edit/branches/dev-zilav/TES5Dump.dpr
@@ -28,7 +28,6 @@ uses
   Windows,
   wbDefinitionsFNV in 'wbDefinitionsFNV.pas',
   wbDefinitionsFO3 in 'wbDefinitionsFO3.pas',
-  wbDefinitionsTES3 in 'wbDefinitionsTES3.pas',
   wbDefinitionsTES4 in 'wbDefinitionsTES4.pas',
   wbDefinitionsTES5 in 'wbDefinitionsTES5.pas',
   wbImplementation in 'wbImplementation.pas',
