3ca051d955c67701897be265eb4313894dd769e2
leandor@gmail.com <leandor@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2012-09-02 06:07:59 +0000

    [TESVSnip] Code-cleanup: Extracted all types inside the ScriptCompiler namespace to a file of each own.

---
+++ TESVSnip/branches/dev-leandor-wip/ScriptCompiler/Keywords.cs
@@ -0,0 +1,39 @@
+namespace ScriptCompiler
+{
+    internal enum Keywords
+    {
+        If, 
+
+        ElseIf, 
+
+        Else, 
+
+        EndIf, 
+
+        ScriptName, 
+
+        Scn, 
+
+        Short, 
+
+        Int, 
+
+        Float, 
+
+        Ref, 
+
+        Begin, 
+
+        End, 
+
+        Set, 
+
+        To, 
+
+        Return, 
+
+        ShowMessage, 
+
+        NotAKeyword
+    }
+}
---
+++ TESVSnip/branches/dev-leandor-wip/ScriptCompiler/TokenType.cs
@@ -0,0 +1,25 @@
+namespace ScriptCompiler
+{
+    internal enum TokenType
+    {
+        Unknown, 
+
+        Integer, 
+
+        Float, 
+
+        Keyword, 
+
+        Symbol, 
+
+        Local, 
+
+        Global, 
+
+        Function, 
+
+        edid, 
+
+        Null
+    }
+}
