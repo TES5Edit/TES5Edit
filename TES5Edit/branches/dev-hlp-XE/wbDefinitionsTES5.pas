3eea36812dc2348f93f6e82889f2ae6dbf7bb00e
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2013-02-05 23:07:16 +0000

    [TES5Edit] Added Unknown TNAM Map Markers to dev-hlp-XE

--- TES5Edit/branches/dev-hlp-XE/wbDefinitionsTES5.pas
+++ TES5Edit/branches/dev-hlp-XE/wbDefinitionsTES5.pas
@@ -12033,6 +12033,12 @@ begin
         50, 'Falkreath Capitol',
         51, 'Dawnstar Castle',
         52, 'Dawnstar Capitol'
+        53, 'DLC02 - Temple of Miraak',
+        54, 'DLC02 - Raven Rock'
+        55, 'DLC02 - Beast Stone'
+        56, 'DLC02 - Tel Mithryn'
+        57, 'DLC02 - To Skyrim'
+        58, 'DLC02 - To Solstheim'
       ]), cpNormal, True)
     ], []),
     {--- Attach reference ---}
