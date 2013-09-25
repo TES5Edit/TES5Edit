2be0df5154e6ad3bde842d04a1bae0f12794bd8d
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2013-09-25 17:08:56 +0000

    [TES5Edit] Adding "Add Prefix Plus Name To EDID" Script

---
+++ TES5Edit/trunk/Delphi XE/Edit Scripts/Add Prefix Plus Name To EDID.pas
@@ -0,0 +1,36 @@
+{
+  This script checkes to see if an EDID is present in the selected record.  
+  If not present it will add an EDID record.  
+  Assign 'Position' to the first space found in NAME
+  Assign the EDID value to be the Prefix, plus the NAME of the reference up to the first space, and a suffix.
+}
+unit AddPrefixPlusNameToEDID;
+
+Const
+  Prefix = 'Reserve';
+
+function Process(e: IInterface): integer;
+var
+  elEditorID: IInterface;
+  s, s1: String;
+  Position, Count: Integer;
+begin
+  Result := 0;
+  Count := 0;
+  s1:= '';
+  AddMessage('Processing: ' + Name(e));
+  elEditorID := ElementByName(e, 'EDID - Editor ID');
+  s := GetElementEditValues(e, 'NAME');
+  Position:= pos(' ', s);
+  if Position > 0 then s1 := Copy(s, 0, Position);
+  if Not Assigned(elEditorID) then Begin
+    elEditorID := Add(e, 'EDID', True)
+	End;
+  Begin
+    //SetEditValue(elEditorID, Prefix + s1 + '_' + IntToStr(Count));
+	SetEditValue(elEditorID, Prefix + s1);
+	Inc(Count)
+  End;
+end;
+
+end.
