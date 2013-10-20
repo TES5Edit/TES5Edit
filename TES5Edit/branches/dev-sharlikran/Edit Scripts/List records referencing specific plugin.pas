2a62e65498e195286fd5108a6e42702ea0155120
HuguesLePors@gmail.com <HuguesLePors@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2013-10-20 10:01:50 +0000

    [Sharlikran] ANd now it is fully synced :(

---
+++ TES5Edit/branches/dev-sharlikran/Edit Scripts/List records referencing specific plugin.pas
@@ -0,0 +1,37 @@
+{
+  Search for references in selected records and check their load order.
+}
+unit UserScript;
+
+var
+  ModLoadOrder: integer;
+
+function Initialize: integer;
+var
+  s: string;
+begin
+  if not InputQuery('Enter', 'Hexadecimal load order of references to search for', s) then 
+    Exit;
+  if s = '' then
+    Exit;
+  ModLoadOrder := StrToInt64('$' + s);
+end;
+
+function Process(e: IInterface): integer;
+var
+  ref: IInterface;
+  i: integer;
+begin
+  Result := 0;
+
+  ref := LinksTo(e);
+  if Assigned(ref) then
+    if GetLoadOrder(GetFile(ref)) = ModLoadOrder then
+      AddMessage(FullPath(e) + ' = ' + Name(ref));
+  
+  if CanContainFormIDs(e) then
+    for i := 0 to ElementCount(e) - 1 do
+      Process(ElementByIndex(e, i));
+end;
+
+end.
