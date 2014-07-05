46917672557555e91ef098544273458bc31ef9c5
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2014-07-05 06:19:49 +0000

    [TES3Edit] second attempt at getting TES3Dump to read the file

--- TES3Edit/trunk/Delphi XE/wbDefinitionsTES3.pas
+++ TES3Edit/trunk/Delphi XE/wbDefinitionsTES3.pas
@@ -4675,7 +4675,17 @@ begin
   ]);
 
   wbRecord(TES3, 'Main File Header', [
-    wbUnknown(HEDR)
+    wbStruct(HEDR, 'Header', [
+      wbFloat('Version'),
+      wbByteArray('Unknown', 4),
+      wbByteArray('Unknown', 32),
+      wbByteArray('Unknown', 256),
+      wbInteger('Number of Records', itU32)
+    ], cpNormal, True),
+    wbRArray('Master Files', wbRStruct('Master File', [
+      wbString(MAST, 'Filename', 0, cpNormal, True),
+      wbByteArray(DATA, 'Unknown', 8, cpIgnore, True)
+    ], []))
   ], False, nil, cpNormal, True);
 
   wbRecord(TREE, 'Tree', [
