cc363383cbba4834787979de6e26fccc09e77219
danielhmpdx16@gmail.com <danielhmpdx16@gmail.com@bdfc8fb3-84cf-a68a-e902-ed0415cf5604>
2014-08-05 02:07:12 +0000

    [TES3Edit] Reorder constants

--- TES3Edit/trunk/Delphi XE/wbDefinitionsTES3.pas
+++ TES3Edit/trunk/Delphi XE/wbDefinitionsTES3.pas
@@ -2991,7 +2991,7 @@ begin
     wbMODL,
     wbString(FNAM, 'Door Name'),
     wbStringScript(SCRI, 'Script Source', 0),
-	{Needs verification, may not have an SCIP record}
+	  {Needs verification, may not have an SCIP record}
     wbStringScript(SCIP, 'Script Source', 0),
     wbString(SNAM, 'Sound name open'),
     wbString(ANAM, 'Sound name close')
