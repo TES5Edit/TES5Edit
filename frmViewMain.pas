b57ac54634cbc24514238d22a34114346a3d62d2
Hugues92 <HuguesLePors@hotmail.com>
2016-01-21 14:45:16 +0100

    [FO4] ERRATA
    This should at least compile and run...

--- frmViewMain.pas
+++ frmViewMain.pas
@@ -7038,9 +7038,6 @@ begin
         StartNode := Selection[i];
         if Assigned(StartNode) then begin
           NodeData := vstNav.GetNodeData(StartNode);
-          if Assigned(NodeData.Element) then
-            if NodeData.Element.ElementType = etFile then
-              wbCurrentSelf := (NodeData.Element as IwbFile).MasterCount;
 
           Node := vstNav.GetLast(StartNode);
           if not Assigned(Node) then
