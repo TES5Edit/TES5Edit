9ee74e8788bcec5788f7e4d4c0a0fdc43a28b50a
eFrysTon <github@thorsten.engler.id.au>
2018-07-30 04:42:05 +1000

    fix for massive slowdown in Virtual TreeView, see https://github.com/Virtual-TreeView/Virtual-TreeView/issues/725

--- wbInterface.pas
+++ wbInterface.pas
@@ -25,7 +25,7 @@ uses
   Graphics;
 
 const
-  VersionString  = '3.2.3u EXPERIMENTAL';
+  VersionString  = '3.2.3v EXPERIMENTAL';
   clOrange       = $004080FF;
   wbFloatDigits  = 6;
   wbHardcodedDat = '.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat';
