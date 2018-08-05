1a60be0c7572494bd873eb9c9c69b7db224db5ca
eFrysTon <github@thorsten.engler.id.au>
2018-08-05 16:55:52 +1000

    increased version to 3.2.11

--- wbInterface.pas
+++ wbInterface.pas
@@ -25,7 +25,7 @@ uses
   Graphics;
 
 const
-  VersionString  = '3.2.10 EXPERIMENTAL';
+  VersionString  = '3.2.11 EXPERIMENTAL';
   clOrange       = $004080FF;
   wbFloatDigits  = 6;
   wbHardcodedDat = '.Hardcoded.keep.this.with.the.exe.and.otherwise.ignore.it.I.really.mean.it.dat';
