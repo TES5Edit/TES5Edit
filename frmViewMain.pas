0d388e7d45aa94a5400c767727c30d68aabb85d9
Sharlikran <danielhmpdx16@gmail.com>
2018-08-19 22:40:59 -0700

    Updated wording

--- frmViewMain.pas
+++ frmViewMain.pas
@@ -3300,7 +3300,7 @@ begin
     SelectFlag := mfTagged;
     FilterFlag := mfHasFile;
     AllowCancel;
-    Caption := 'In the .modgroups file of which module should the new ModGroup be stored?';
+    Caption := 'In which .modgroups file should the new ModGroup be stored?';
     MaxSelect := 1;
     MinSelect := 1;
     repeat
