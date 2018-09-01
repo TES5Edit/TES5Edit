3dcd07eb4c215c49cde5f1a7838cefe14a5a24a8
eFrysTon <github@thorsten.engler.id.au>
2018-09-02 02:01:04 +1000

    increased version to 3.2.59 EXPERIMENTAL

--- wbInterface.pas
+++ wbInterface.pas
@@ -25,7 +25,7 @@ uses
   Graphics;
 
 const
-  VersionString            = '3.2.58 EXPERIMENTAL';
+  VersionString            = '3.2.59 EXPERIMENTAL';
   wbDevCRC32App : Cardinal = $FFFFFFF0;
 
   clOrange       = $004080FF;
