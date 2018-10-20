84a069e4d5b5772097321173c05791037fa35c51
eFrysTon <github@thorsten.engler.id.au>
2018-10-21 00:21:31 +1000

    changed "Next Object ID" field in file header to show as hex and allow setting to ? to automatically set to highest ObjectID in file +1

--- wbDefinitionsFNV.pas
+++ wbDefinitionsFNV.pas
@@ -10677,7 +10677,7 @@ begin
     wbStruct(HEDR, 'Header', [
       wbFloat('Version'),
       wbInteger('Number of Records', itU32),
-      wbInteger('Next Object ID', itU32)
+      wbInteger('Next Object ID', itU32, wbNextObjectIDToString, wbNextObjectIDToInt)
     ], cpNormal, True),
     wbByteArray(OFST, 'Unknown', 0, cpIgnore),
     wbByteArray(DELE, 'Unknown', 0, cpIgnore),
