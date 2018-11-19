e7e6b0f4c7922d90bf52104ac46ba48f1f3ceb4c
Jonathan Ostrus <jostrus@gmail.com>
2018-11-18 21:59:59 -0800

    removed extraneous 'dxGDIPlusClasses' use.

--- frmDeveloperMessageForm.pas
+++ frmDeveloperMessageForm.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
-  JvExStdCtrls, JvRichEdit, FileContainer, dxGDIPlusClasses, JvHtControls;
+  JvExStdCtrls, JvRichEdit, FileContainer, JvHtControls;
 
 type
   TfrmDeveloperMessage = class(TForm)
