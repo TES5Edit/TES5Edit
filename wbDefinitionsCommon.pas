97503c0e09d1e8e9061c104d2f6dbe490d72390c
fireundubh <fireundubh@gmail.com>
2019-12-10 03:22:03 -0800

    Moved wbMainRecordHeader to wbDefinitionsCommon (TES5, SSE)

--- wbDefinitionsCommon.pas
+++ wbDefinitionsCommon.pas
@@ -213,7 +213,7 @@ function wbScriptObjFormatDecider(aBasePtr: Pointer; aEndPtr: Pointer; const aEl
 
 {>>> Common Definitions <<<}
 
-function wbRecordHeader(aRecordFlags: IwbIntegerDef; aVCIToStrCallback: TwbToStrCallback): IwbValueDef;
+function wbRecordHeader(aRecordFlags: IwbIntegerDef; aVCIToStrCallback: TwbToStrCallback = nil): IwbValueDef;
 
 function wbClimateTiming(aTimeCallback: TwbIntToStrCallback; aPhaseCallback: TwbIntToStrCallback): IwbRecordMemberDef;
 
@@ -1078,16 +1078,25 @@ end;
 
 {>>> Common Definitions <<<}
 
-function wbRecordHeader(aRecordFlags: IwbIntegerDef; aVCIToStrCallback: TwbToStrCallback): IwbValueDef;
+function wbRecordHeader(aRecordFlags: IwbIntegerDef; aVCIToStrCallback: TwbToStrCallback = nil): IwbValueDef;
 begin
+  if not Assigned(aVCIToStrCallback) and not (wbGameMode in [gmTES5, gmSSE]) then
+    raise Exception.Create('wbRecordHeader - aVCIToStrCallback not assigned');
+
   Result := wbStruct('Record Header', [
-    {00} wbString('Signature', 4, cpCritical),
-    {04} wbInteger('Data Size', itU32, nil, cpIgnore),
-    {08} aRecordFlags,
-    {12} wbFormID('FormID', cpFormID).IncludeFlag(dfSummarySelfAsShortName),
-    {16} wbByteArray('Version Control Info 1', 4, cpIgnore).SetToStr(aVCIToStrCallback),
-    {20} wbInteger('Form Version', itU16, nil, cpIgnore).IncludeFlag(dfSummaryShowIgnore),
-    {22} wbByteArray('Version Control Info 2', 2, cpIgnore)
+    wbString('Signature', 4, cpCritical),
+    wbInteger('Data Size', itU32, nil, cpIgnore),
+    aRecordFlags,
+    wbFormID('FormID', cpFormID).IncludeFlag(dfSummarySelfAsShortName),
+    IfThen(wbGameMode in [gmTES5, gmSSE],
+      wbUnion('Version Control Info 1', wbFormVersionDecider(44), [
+        wbByteArray('Version Control Info 1', 4, cpIgnore).SetToStr(wbVCI1ToStrBeforeFO4),
+        wbByteArray('Version Control Info 1', 4, cpIgnore).SetToStr(wbVCI1ToStrAfterFO4)
+      ]),
+      wbByteArray('Version Control Info 1', 4, cpIgnore).SetToStr(aVCIToStrCallback)
+    ),
+    wbInteger('Form Version', itU16, nil, cpIgnore).IncludeFlag(dfSummaryShowIgnore),
+    wbByteArray('Version Control Info 2', 2, cpIgnore)
   ])
   .SetSummaryKey([5, 3, 2])
   .SetSummaryMemberPrefixSuffix(5, '[v', ']')
