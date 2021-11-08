d776a10cb0c11bdd1c816dea9d8a3c10f6afcd6f
ElminsterAU <github@thorsten.engler.id.au>
2021-11-08 22:54:23 +1000

    added confirmation dialog for canceling ongoing operation
    (cherry picked from commit 41939fac02bdbb7e356cd7ec785ed4409c970c2f)

--- frmViewMain.pas
+++ frmViewMain.pas
@@ -10666,7 +10666,9 @@ end;
 
 procedure TfrmMain.btnCancelClick(Sender: TObject);
 begin
-  wbForceTerminate := True;
+  if MessageDlg('Are you sure you want to request to force cancel the current operation?' + sLineBreak + sLineBreak +
+    'Canceling the operation might leave xEdit in an inconsistent state.', mtConfirmation, mbYesNo, 0, mbNo) = mrYes then
+    wbForceTerminate := True;
 end;
 
 procedure TfrmMain.mniNavUndeleteAndDisableReferencesClick(Sender: TObject);
