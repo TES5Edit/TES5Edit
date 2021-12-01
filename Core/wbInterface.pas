1dc18d001359bd68644380764be5e28edf1a2c91
ElminsterAU <github@thorsten.engler.id.au>
2021-12-02 03:03:34 +1000

    increased version to 4.1.4b

--- Core/wbInterface.pas
+++ Core/wbInterface.pas
@@ -47,12 +47,12 @@ var
     Major   : 4;
     Minor   : 1;
     Release : 4;
-    Build   : 'a';
+    Build   : 'b';
     Title   : 'EXTREMELY EXPERIMENTAL';
   );
 
 const
-  wbWhatsNewVersion : Integer = 04010401;
+  wbWhatsNewVersion : Integer = 04010402;
   wbDeveloperMessageVersion : Integer = 04010400;
   wbDevCRC32App : Cardinal = $FFFFFFE7;
 
