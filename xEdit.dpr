e25596f5f432b3b3cb096fd49d57dc09cca77d23
ElminsterAU <github@thorsten.engler.id.au>
2022-08-23 13:28:06 +1000

    enabled insertJDBG

--- xEdit.dpr
+++ xEdit.dpr
@@ -1,5 +1,6 @@
 {******************************************************************************
-
+
+
   This Source Code Form is subject to the terms of the Mozilla Public License,
   v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain
   one at https://mozilla.org/MPL/2.0/.
@@ -10,8 +11,8 @@
 
 {$IFDEF EXCEPTION_LOGGING_ENABLED}
 // JCL_DEBUG_EXPERT_GENERATEJDBG OFF
-// JCL_DEBUG_EXPERT_INSERTJDBG OFF
-// JCL_DEBUG_EXPERT_DELETEMAPFILE OFF
+// JCL_DEBUG_EXPERT_INSERTJDBG ON
+// JCL_DEBUG_EXPERT_DELETEMAPFILE ON
 {$ENDIF}
 
 program xEdit;
