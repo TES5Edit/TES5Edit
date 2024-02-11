b976aa6577cce13f5112dbb677cb70d7207c497f
ElminsterAU <github@thorsten.engler.id.au>
2024-02-11 17:41:11 +1000

    increased version to 4.1.5c updated whatsnew.rtf

--- Core/wbInterface.pas
+++ Core/wbInterface.pas
@@ -58,13 +58,13 @@ var
     Major   : 4;
     Minor   : 1;
     Release : 5;
-    Build   : 'b';
+    Build   : 'c';
     Title   : 'EXTREMELY EXPERIMENTAL';
   );
 
 const
-  wbWhatsNewVersion : Integer = 04010502;
-  wbDeveloperMessageVersion : Integer = 04010500;
+  wbWhatsNewVersion : Integer = 04010503;
+  wbDeveloperMessageVersion : Integer = 04010503;
   wbDevCRC32App : Cardinal = $FFFFFFE7;
 
   clOrange       = $004080FF;
