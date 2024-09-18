0e4b23cbe9fa9b0712ed1103f8fd68e18f4fbc26
robertgk2017 <robertgk2017@gmail.com>
2024-09-18 02:06:38 -0400

    Not just yet on Starfield Form Version 576, ck doesn't support it yet.

--- Core/wbImplementation.pas
+++ Core/wbImplementation.pas
@@ -9785,7 +9785,7 @@ var
 
     if wbGameMode >= gmFO3 then begin
       case wbGameMode of
-        gmSF1                        : BasePtr.mrsVersion^ := 576;
+        gmSF1                        : BasePtr.mrsVersion^ := 575;
         gmFO76                       : BasePtr.mrsVersion^ := 201;
         gmFO4, gmFO4VR               : BasePtr.mrsVersion^ := 131;
         gmSSE, gmTES5VR, gmEnderalSE : BasePtr.mrsVersion^ := 44;
