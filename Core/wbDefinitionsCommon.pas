f12b48c0fd4c18f10d63267ac77061a17a0d6bf4
ElminsterAU <github@thorsten.engler.id.au>
2026-09-06 17:38:06 +1000

    Resolve the switch group a Wwise switch field's parent names by its complete display string first, and by the bare group name only when exactly one accessible group carries it, where both the edit value and the edit info arms stripped the parent's display string to the bare name and took the first prefix match in the sorted list of accessible groups, so a second module declaring a switch group under a vanilla group's name shadowed the vanilla one and the field was offered, and could only be given, that module's switches

--- Core/wbDefinitionsCommon.pas
+++ Core/wbDefinitionsCommon.pas
@@ -5428,14 +5428,57 @@ begin
   aValue := '' + '(' + X + ', ' + Y + ', ' + Z + ')';
 end;
 
+function wbWwiseSwitchGroupByDisplay(const aElement: IwbElement; const aDisplay: string; out aGUID: TGUID): Boolean;
+var
+  lMasters, lGroups : TStringList;
+  lName             : string;
+  lPos, lMatches    : Integer;
+  lCandidate        : TGUID;
+  lFile             : IwbFile;
+begin
+  Result := wbSoundBankCache.TryLookupDisplay(wntSwitchGroup, aDisplay, aGUID);
+  if Result then
+    Exit;
+
+  lPos := Pos(' [', aDisplay);
+  if lPos > 0 then
+    lName := Copy(aDisplay, 1, lPos - 1)
+  else
+    lName := aDisplay;
+
+  lMasters := TStringList.Create;
+  lGroups := TStringList.Create;
+  try
+    lFile := aElement._File;
+    if Assigned(lFile) then begin
+      lFile.GetMasters(lMasters);
+      lMasters.Add(lFile.FileName);
+    end;
+
+    wbSoundBankCache.GetStrings(wntSwitchGroup, lMasters, lGroups);
+
+    lMatches := 0;
+    for var I := 0 to Pred(lGroups.Count) do
+      if StartsText(lName + ' [', lGroups[I]) and wbSoundBankCache.TryLookupDisplay(wntSwitchGroup, lGroups[I], lCandidate) then
+      begin
+        Inc(lMatches);
+        aGUID := lCandidate;
+      end;
+
+    Result := lMatches = 1;
+  finally
+    lGroups.Free;
+    lMasters.Free;
+  end;
+end;
+
 procedure wbWwiseGuidToStr(var aValue:string; aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement; aType: TwbCallbackType);
 var
-  lBool: Boolean;
   lIndex, lPos: Integer;
   lGUID: TGUID;
   lNodeType: TwbWwiseNodeType;
-  lList1, lList2, lList3: TStringList;
-  lString1, lString2, lString3: string;
+  lList1, lList2: TStringList;
+  lString1, lString2: string;
   lDef: IwbNamedDef;
   lElement: IwbElement;
   lFile: IwbFile;
@@ -5535,45 +5578,8 @@ begin
           end;
 
           if lString2 <> '' then
-          begin
-            lPos := Pos(' [', lString2);
-            if lPos > 0 then
-              lString3 := Copy(lString2, 1, lPos - 1)
-            else
-              lString3 := lString2;
-
-            lList2 := TStringList.Create;
-            lList3 := TStringList.Create;
-            try
-              lFile := aElement._File;
-              if Assigned(lFile) then begin
-                lFile.GetMasters(lList2);
-                lList2.Add(lFile.FileName);
-              end;
-
-              wbSoundBankCache.GetStrings(wntSwitchGroup, lList2, lList3);
-
-              lBool := False;
-              for lIndex := 0 to Pred(lList3.Count) do
-              begin
-                if StartsText(lString3 + ' [', lList3[lIndex]) then
-                begin
-                  if wbSoundBankCache.TryLookupDisplay(wntSwitchGroup, lList3[lIndex], lGUID) then
-                  begin
-                    lBool := True;
-                    Break;
-                  end;
-                end;
-              end;
-
-              if lBool then
-                wbSoundBankCache.GetChildStrings(lGUID, wntSwitch, lList1);
-
-            finally
-              lList2.Free;
-              lList3.Free;
-            end;
-          end;
+            if wbWwiseSwitchGroupByDisplay(aElement, lString2, lGUID) then
+              wbSoundBankCache.GetChildStrings(lGUID, wntSwitch, lList1);
         end
         else
         begin
@@ -5623,46 +5629,8 @@ begin
             lElement := aElement.Container.ElementByPath[lParentNodePath];
 
           if Assigned(lElement) and (lElement.EditValue <> '') then
-          begin
-            lString1 := lElement.EditValue;
-            lPos := Pos(' [', lString1);
-            if lPos > 0 then
-            begin
-              lString2 := Copy(lString1, 1, lPos - 1);
-
-              lList2 := TStringList.Create;
-              lList3 := TStringList.Create;
-              try
-                lFile := aElement._File;
-                if Assigned(lFile) then begin
-                  lFile.GetMasters(lList2);
-                  lList2.Add(lFile.FileName);
-                end;
-
-                wbSoundBankCache.GetStrings(wntSwitchGroup, lList2, lList3);
-
-                lBool := False;
-                for lIndex := 0 to Pred(lList3.Count) do
-                begin
-                  if StartsText(lString2 + ' [', lList3[lIndex]) then
-                  begin
-                    if wbSoundBankCache.TryLookupDisplay(wntSwitchGroup, lList3[lIndex], lGUID) then
-                    begin
-                      lBool := True;
-                      Break;
-                    end;
-                  end;
-                end;
-
-                if lBool then
-                  wbSoundBankCache.GetChildStrings(lGUID, wntSwitch, lList1);
-
-              finally
-                lList2.Free;
-                lList3.Free;
-              end;
-            end;
-          end;
+            if wbWwiseSwitchGroupByDisplay(aElement, lElement.EditValue, lGUID) then
+              wbSoundBankCache.GetChildStrings(lGUID, wntSwitch, lList1);
         end
         else
         begin
